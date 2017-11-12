5e47a83a35fb49e64e7263869fa041b9416fac32
Alexey R.B <arb.rolon@gmail.com>
2017-11-12 22:56:18 +0100

    Crappy output from step function

--- src/Controller.hs
+++ src/Controller.hs
@@ -254,8 +254,13 @@ handleCollisionHelper colisionType gstate maxDepth
 
 step :: Float -> GameState -> IO GameState
 step frameTime gstate
-	| paused gstate 	= return $ gstate
-	| otherwise 		= return $ newGameState
+	| paused gstate 	= do
+                            return $ gstate
+	| otherwise 		= do
+                            outputFile <- return ("src/output.txt" :: FilePath)
+                            appendFile outputFile ("Time elapsed:" ++ show (elapsedTime gstate) ++ "\n")
+                            print (show $ elapsedTime gstate)
+                            return $ newGameState
 	where
 		-- Update player positions without gravity
 		playerObject	= player gstate
